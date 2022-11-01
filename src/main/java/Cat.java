a278ec0b35873883427b093f7326bfbe25e57ea7
newdecada <105361021+newdecada@users.noreply.github.com>
2022-11-01 18:23:35 +0300

    Add files via upload

---
+++ src/main/java/Cat.java
@@ -0,0 +1,13 @@
+
+
+public class Cat {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String name) {
+        this.message = name;
+    }
+}
